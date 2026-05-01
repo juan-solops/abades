0cc9646de444dfb5d275c4311b480d8e019a7189
juan-solops <casajuanlau@gmail.com>
2026-05-01 23:38:48 +0200

    sx

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const navToggle = document.getElementById("navToggle");
 const mainNav = document.getElementById("mainNav");
 const year = document.getElementById("year");
-const languageSelects = document.querySelectorAll(".language-select");
-
-languageSelects.forEach((select) => {
-  select.addEventListener("change", () => {
-    window.location.href = select.value;
-  });
-});
-
 
 if (navToggle && mainNav) {
   navToggle.addEventListener("click", () => {
